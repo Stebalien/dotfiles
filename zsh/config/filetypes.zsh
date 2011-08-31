e0b8a952392d77830e11314c30bb241972a7271d
Steven Allen <steven@stebalien.com>
2011-08-31 12:17:21 -0700

    Update zsh config.

--- zsh/config/filetypes.zsh
+++ zsh/config/filetypes.zsh
@@ -4,5 +4,7 @@ zstyle ':mime:.pkg.tar.xz:' flags 'needsterminal'
 
 zstyle ':mime:.src.tar.gz:' handler '/usr/bin/aunpack %s'
 zstyle ':mime:.src.tar.gz:' flags 'needsterminal'
+
+zstyle ':mime:.jar:' handler '/usr/bin/java -jar %s'
 zsh-mime-setup
 
